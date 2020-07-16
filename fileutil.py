4d232a13319277c91bf6a69a6ef168d0efb54442
Kaveh Karbasi <kkarbasi@berkeley.edu>
2020-07-15 19:22:18 -0700

    Update fileutil.py

--- fileutil.py
+++ fileutil.py
@@ -1,14 +1,28 @@
-def _find_recursive(base_dir, extention = 'mff', ftype = 'd'):
+def find_recursive(base_dir, extention = 'mff', ftype = 'd', max_depth = None):
     """
     Finds files or directories with the specified extention
     """
     import os
+    def walk(top, maxdepth):
+        dirs, nondirs = [], []
+        for name in os.listdir(top):
+            (dirs if os.path.isdir(os.path.join(top, name)) else nondirs).append(name)
+        yield top, dirs, nondirs
+        if maxdepth > 1:
+            for name in dirs:
+                for x in walk(os.path.join(top, name), maxdepth-1):
+                    yield x
+    if max_depth == None:
+        walker = os.walk
+    else:
+        walker = walk
+        
     list_of_files = []
     if ftype == 'd':
         try:
-            for root, dirnames, filenames in os.walk(base_dir):
+            for root, dirnames, filenames in walker(base_dir, max_depth):
                 for d_name in dirnames:
-                    if d_name.endswith(extention):
+                    if d_name.endswith(extention) and d_name.startswith('SHWK'):
                         list_of_files.append(os.path.join(root, d_name))
                         print('Found-->{}'.format(list_of_files[-1]))
         except Exception as e:
@@ -16,7 +30,7 @@ def _find_recursive(base_dir, extention = 'mff', ftype = 'd'):
             pass
     if ftype == 'f':
         try:
-            for root, dirnames, filenames in os.walk(base_dir):
+            for root, dirnames, filenames in walker(base_dir, max_depth):
                 for f_name in filenames:
                     if f_name.endswith(extention):
                         list_of_files.append(os.path.join(root, f_name))
